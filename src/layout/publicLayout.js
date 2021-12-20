8f65deb46a56d83169fe796ef54194318d70a044
a14rathore <87847257+a14rathore@users.noreply.github.com>
2021-12-20 12:06:59 +0530

    first commit

--- src/layout/publicLayout.js
+++ src/layout/publicLayout.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-
+import Counter from "../pages/public/Counter/counter.js";
 export default function PublicLayout({ children }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -13,6 +13,7 @@ export default function PublicLayout({ children }) {
           {children}
         </Grid>
       </Grid>
+      <Counter />
     </Box>
   );
 }
---
+++ src/pages/public/Counter/counter.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    const btn = {
+        padding: '10px',
+        margin: '3px 4px',
+        width: '3pc'
+    }
+    return (
+        <div style={{ display: 'block', height: '200px', width: '200px', margin: 'auto' }}>
+            <div style={{ display: 'flex', justifyContent: 'centre' }}>
+                <button style={btn} onClick={() => { setCount(pre => pre + 1) }}>+</button>
+                <h4 style={{ padding: '5px', margin: '5px', fontSize: '2pc' }}>{count}</h4>
+                <button style={btn} onClick={() => { setCount(pre => pre - 1) }}>-</button>
+
+            </div>
+
+        </div>
+    )
+}
