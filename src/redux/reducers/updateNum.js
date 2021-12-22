ff4981f35843ee9935c72f27881ae42eaee49e53
Akshay Rathore <akshayrathore801@gmail.com>
2021-12-22 17:59:22 +0530

    bug fix

--- src/redux/reducers/updateNum.js
+++ src/redux/reducers/updateNum.js
@@ -3,7 +3,7 @@ const initialState = 0;
 const UpdateNum = (state = initialState, action) => {
     switch (action.type) {
         case "Increase": return state + action.payload;
-        case "Decrease": return state - 1;
+        case "Decrease": return state - 2;
         default: return state;
     }
 
