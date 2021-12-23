816e05a2b52656050662b4763aed944482b227c0
Akshay Rathore <akshayrathore801@gmail.com>
2021-12-23 17:26:55 +0530

    route path  counter

--- src/router/routes/publicRoutes.js
+++ src/router/routes/publicRoutes.js
@@ -1,9 +1,11 @@
 // Export all the public routes
 
+import Counter from "@pages/public/Counter/counter";
 import Login from "@pages/public/login";
 import SignUp from "@pages/public/signup";
 
 export const PublicRoutes = [
   { path: "/auth/login", exact: true, component: Login },
-  { path: "/auth/signup", exact: true, component: SignUp }
+  { path: "/auth/signup", exact: true, component: SignUp },
+  { path: "/auth/counter", exact: true, component: Counter },
 ];
