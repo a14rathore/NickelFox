4d71cd23d2ff462a1e10179a7616bf9b8d3a8c2b
Akshay Rathore <akshayrathore801@gmail.com>
2021-12-23 17:30:44 +0530

    signUp Page

--- src/layout/publicLayout.js
+++ src/layout/publicLayout.js
@@ -1,19 +1,16 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Counter from "../pages/public/Counter/counter.js";
+// import Counter from "../pages/public/Counter/counter.js";
 export default function PublicLayout({ children }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={7}>
-          <h2>Some placeholder</h2>
-        </Grid>
-        <Grid item xs={5}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} >
           {children}
         </Grid>
       </Grid>
-      <Counter />
+
     </Box>
   );
 }
--- src/pages/public/signup/index.js
+++ src/pages/public/signup/index.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './signUp.css';
 
 const SignUp = () => {
-  return <div></div>;
+  const [data, setdata] = useState({
+    Name: "",
+    Email: "",
+    "phone No": "",
+    Password: "",
+    RePassword: "",
+  });
+  const localStorageData = localStorage.getItem("list")
+  const [array, setArray] = useState(localStorageData ? JSON.parse(localStorageData) : []);
+  const onChangeHandler = (e) => {
+
+    const { placeholder, value } = e.target;
+    setdata((pre) => {
+      return {
+        ...pre,
+        [placeholder]: value,
+      }
+    });
+
+  }
+  const setDataToLocalStoragre = (e) => {
+    e.preventDefault();
+    (data.Name && data.Password && data.RePassword && data["phone No"] && data.Email) ?
+      (
+        setArray([...array, data]),
+        setdata({
+          Name: "",
+          Email: "",
+          "phone No": "",
+          Password: "",
+          RePassword: "",
+        })
+      )
+      : alert("cant be Empty")
+  }
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(array))
+  })
+  return (
+    <>
+      <form className="form">
+        <h1>SignUp here</h1>
+        <div className="filled_div">
+          <label className="label">Name</label>
+          <input type="text" placeholder="Name" value={data.Name} onChange={onChangeHandler} />
+          <label className="label">Email</label>
+          <input type="email" placeholder="Email" value={data.Email} onChange={onChangeHandler} />
+          <label className="label">Phone No</label>
+          <input type="tel" placeholder="phone No" value={data["phone No"]} onChange={onChangeHandler} />
+          <label className="label">Password</label>
+          <input type="password" placeholder="Password" value={data.Password} onChange={onChangeHandler} />
+          <label className="label">Re-Password</label>
+          <input type="password" placeholder="RePassword" value={data.RePassword} onChange={onChangeHandler} />
+          {data.Password !== data.RePassword ? <p>Password doesn&apos;t match</p> : ""}
+          <button className="Btn" onClick={setDataToLocalStoragre}>SignUp</button>
+
+        </div>
+
+      </form>
+    </>
+
+  )
+
 };
 
 export default SignUp;
